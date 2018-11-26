9a6eb443ab3fcc681aac07dba3a4a21510be30cd
joseS2W <josealbertoriera@iespaucasesnoves.cat>
2018-11-26 20:59:46 +0100

    segundo commit

--- autores.php
+++ autores.php
@@ -17,6 +17,9 @@
   input{
       width: 50px;
   }
+  .ancho{
+      width: 200px;
+  }
 </style>
  </head>
  <body>
@@ -35,17 +38,32 @@ if ($mysqli->connect_error) {
 echo "Se ha podido conectar correctamente";
 echo "<br>";
 $mysqli->set_charset("utf8");
+/*Variable oculta con  el autor*/
+$ordenarpor = 'ID_AUT';
+/*Variable oculta con el orden*/
+$ordenarcon= 'ASC';
+/*Comprovamos si tienen valores*/
+if (isset($_POST['ordenarpor'])) {
+    $ordenarpor = $_POST['ordenarpor'];
+}
+if (isset($_POST['ordenarcon'])) {
+    $ordenarcon = $_POST['ordenarcon'];
+}
+/*Filtro */
+$filtra = "";
+$busqueda=(isset($_POST['busqueda'])? $_POST['busqueda'] : '');
+if($busqueda != ''){
+    $filtra = "where ID_AUT = '$busqueda' OR NOM_AUT like '%$busqueda%'";
+}
 
 ?>
-<form name="formulario" action="" method="POST" >
 
+<form name="formulario" action="" method="POST" >
    <input type="submit" name="Ascendente"  value="A-Z"  />
    <input type="submit" name="Descendente"  value="Z-A" /><BR>
-
    <input type="submit" name="codiAsc"  value="0-9"  />
    <input type="submit" name="codiDesc"  value="9-0" /><BR>
-
-</form>
+</form><br>
 <?php
 $ordenar = "NOM_AUT ASC";
 if(isset($_POST['Ascendente'])){
@@ -60,27 +78,39 @@ if(isset($_POST['codiAsc'])){
 if(isset($_POST['codiDesc'])){
     $ordenar = "ID_AUT DESC";
 }
-$query = "SELECT ID_AUT, NOM_AUT FROM autors ORDER BY $ordenar LIMIT 0 , 20";
-
+$query = "SELECT ID_AUT, NOM_AUT FROM autors $filtra";
+$query .= " ORDER BY $ordenar LIMIT 0 , 20";
 echo "<table border = 1 px>";
-
 if ($cursor = $mysqli->query($query)) {
     while ($row = $cursor->fetch_assoc()) {
         echo "<tr><td>" . $row["ID_AUT"] . "</td><td>" . $row["NOM_AUT"] . "</td></tr>";
     }
     $cursor->free();
 }
+/*Input amb el cerca de nom o codi*/
+?>
 
+<form name="buscar" action="" method="POST" >
+<fieldset>
+    <legend>Introduzca un numero <br>o un nombre<br>para buscar</legend>
+    <input type ="text" name="busqueda" class="ancho" />
+    <input type="submit" value="cercarID" >
+    </fieldset>
+</form>
+
+
+<?php
+/*Filtrar lo que llega*/
+
+/*10
 
 $mysqli->close();
 echo" Query utilizada para el orden<br>
 SELECT `ID_AUT`,`NOM_AUT` FROM `autors` ORDER BY `NOM_AUT` DESC LIMIT 0,10";
-
+*/
 
 
 ?>
 
  </body>
  </html>
-
-
