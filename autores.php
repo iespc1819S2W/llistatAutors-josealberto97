02f45c669728979d591e21a20651ce4445b1e051
joseS2W <josealbertoriera@iespaucasesnoves.cat>
2018-11-28 20:51:54 +0100

    paginacion

--- autores.php
+++ autores.php
@@ -32,31 +32,12 @@ session_unset();
 </header>
 
  <?php
-$mysqli = new mysqli("localhost","biblioteca","Calamorell","biblioteca");
 
-// Comprobar conexion
-if ($mysqli->connect_error) {
-    die("Conexion fallida: " . $mysqli->connect_error);
-}
-echo "Se ha podido conectar correctamente";
-echo "<br>";
-$mysqli->set_charset("utf8");
-
-/*Filtro */
-$filtra = "";
-$busqueda=(isset($_POST['busqueda'])? $_POST['busqueda'] : '');
-if($busqueda != ''){
-    $filtra = "where ID_AUT = '$busqueda' OR NOM_AUT like '%$busqueda%'";
-}
-?>
-<form name="formulario" action="" method="POST" >
-   <input type="submit" name="Ascendente"  value="A-Z"  />
-   <input type="submit" name="Descendente"  value="Z-A" /><BR>
-   <input type="submit" name="codiAsc"  value="0-9"  />
-   <input type="submit" name="codiDesc"  value="9-0" /><BR>
-</form><br>
-<?php
 $ordenar = "NOM_AUT ASC";
+if(isset($_POST['ordenar'])){
+    $ordenar=$_POST['ordenar'];
+}
+
 if(isset($_POST['Ascendente'])){
     $ordenar = "NOM_AUT ASC";
 }
@@ -70,39 +51,110 @@ if(isset($_POST['codiDesc'])){
     $ordenar = "ID_AUT DESC";
 }
 
-$resultados=10;
-if (isset($_GET["pagina"])) {
-    if (is_numeric($_GET["pagina"])) {
-        if ($_GET["pagina"] == 1) {
-            $pagina=1;
-        }else{
-          $pagina=$_GET["pagina"];  
-        }
-    }
-}else{
-    $pagina=1;
+
+
+$mysqli = new mysqli("localhost","biblioteca","Calamorell","biblioteca");
+
+// Comprobar conexion
+if ($mysqli->connect_error) {
+    die("Conexion fallida: " . $mysqli->connect_error);
 }
-$inicio=($pagina-1)*$resultados;
+echo "Se ha podido conectar correctamente";
+echo "<br>";
+$mysqli->set_charset("utf8");
+
+
+/*Filtro */
+$filtra = "";
+$busqueda=(isset($_POST['busqueda'])? $_POST['busqueda'] : '');
+if($busqueda != ''){
+    $filtra = "where ID_AUT = '$busqueda' OR NOM_AUT like '%$busqueda%'";
+}
+
+
+/**/
+if(isset($_POST['calculopaginas'])){
+    $total_paginas=$_POST['calculopaginas'];
+}else{
+    $total_paginas=1;
+}
+
+/*Creamos el contenido de  calcular las pag*/
+if(isset($_POST['filtro'])){
+    $mantener= $_POST['filtro'];
+}else{
+    $mantener=0;
+}
+$resultados=20;
+$pagina=0;
+$sql2="select count(*) as total from autors $filtra";
+    $cursor3=$mysqli->query($sql2)or die ($sql2);
+    if($fila2=$cursor3->fetch_assoc()){
+        $finicher2=$fila2['total'];
+       $pagina= ceil($finicher2/$resultados);
+    }
+
+
+
+/*Calcular el siguiente*/
+if(isset($_POST['siguiente'])){
+    if(!($total_paginas==$pagina)){
+
+
+    $mantener=$mantener+$resultados;
+    $total_paginas=$total_paginas+1;
+    }
+}
+if(isset($_POST['anterior'])){
+
+    if($total_paginas!= 1){
+    $mantener=$mantener-$resultados;
+    $total_paginas=$total_paginas-1;
+    }
+}
+if(isset($_POST['inicial'])){
+    $mantener=0;
+    $total_paginas=1;
+
+}
+if(isset($_POST['final'])){
+    
+    $sql="select count(*) as total from autors $filtra";
+    $cursor2=$mysqli->query($sql)or die ($sql);
+    if($fila=$cursor2->fetch_assoc()){
+        $finicher=$fila['total'];
+    }
+    $mantener=$finicher-$resultados;
+    $total_paginas= ceil($finicher/$resultados);
+}
+
+
+
+
+
+
+
+
+
 
 $query = "SELECT ID_AUT, NOM_AUT FROM autors $filtra";
-$query .= " ORDER BY $ordenar LIMIT $inicio , $resultados";
+$query .= " ORDER BY $ordenar LIMIT $mantener , $resultados";
 
-/*Obtenemos los datos de la consulta global*//*
-if(!isset($_SESSION["principal"])){
-$principal=$query;
-$_SESSION["principal"]=$principal;
-}else{
-    $principal=$_SESSION["principal"];
-}*/
+/*Obtenemos los datos de la consulta global
+if($_POST['busqueda'] = ''){
 $principal="SELECT ID_AUT, NOM_AUT FROM autors $filtra";
+}else{
+    $principal=$query;
+}
+
 echo"hola".$principal;
 $consulta_global= mysqli_query($mysqli,$principal);
-/*Todos los resultados*/
+Todos los resultados
 $total_registros=mysqli_num_rows($consulta_global);
-/*Obtener el total de paginas*/
-/*Registros/el limite que quiero*/
+Obtener el total de paginas
+Registros/el limite que quiero
 $total_paginas= ceil($total_registros/$resultados);
-
+*/
 
 echo "<table border = 1 px>";
 if ($cursor = $mysqli->query($query)) {
@@ -113,19 +165,37 @@ if ($cursor = $mysqli->query($query)) {
 }
 /*Input amb el cerca de nom o codi*/
 ?>
+<form name="formulario" action="" method="POST" >
+   <input type="submit" name="Ascendente"  value="A-Z"  />
+   <input type="submit" name="Descendente"  value="Z-A" /><BR>
+   <input type="submit" name="codiAsc"  value="0-9"  />
+   <input type="submit" name="codiDesc"  value="9-0" /><BR>
+   
+   <input type="hidden" name="calculopaginas" value="<?=$total_paginas?>"/>
+   <input type="hidden" name="ordenar" value="<?=$ordenar?>"/>
+   <input type="hidden" name="filtro" value="<?=$mantener?>"/>
 
+    <input type="submit" name="inicial" value="<<"/>
+    <input type="submit" name="anterior" value="<"/>
+    <input type="submit" name="siguiente" value=">"/>
+    <input type="submit" name="final" value=">>"/>
+    <br>
+    <input type ="text" name="busqueda" class="ancho" value="<?=$busqueda?>"/>
+    <input type="submit" value="cercarID" >
+</form><br>
+
+<?php
+echo " $total_paginas"."/"."$pagina";
+/*
 <form name="buscar" action="" method="POST" >
 <fieldset>
     <legend>Introduzca un numero <br>o un nombre<br>para buscar</legend>
     <input type ="text" name="busqueda" class="ancho" />
     <input type="submit" value="cercarID" >
     </fieldset>
-</form><br>
-<?php
-for ($i=1; $i<=$total_paginas; $i++) {
-	//En el bucle, muestra la paginación
-	echo "<a href='?pagina=".$i."'>".$i."</a> | ";
-}; ?>
+</form><br>*/
+?>
+
 
  </body>
  </html>
